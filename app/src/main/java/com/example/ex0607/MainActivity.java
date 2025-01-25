e4f1af5214cf08cd8b9e7cf4a432c20492b19a08
sh7478 <sh7478@bs.amalnet.k12.il>
2025-01-25 21:28:15 +0200

    added random number, onclick function, and imagebutton variable

--- app/src/main/java/com/example/ex0607/MainActivity.java
+++ app/src/main/java/com/example/ex0607/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ex0607;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +10,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton imageBtn;
+    int randomNum;
+    Random rnd = new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageBtn = findViewById(R.id.imageBtn);
+    }
+
+    public void clicked(View view)
+    {
+        randomNum = rnd.nextInt(3+1-1)+1;
+
     }
 }
